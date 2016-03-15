773dca103ee313f71710d5d98317bf3a6eab76cf
Raphael Negrisoli Batista <raphael.batista@bcash.com.br>
2016-03-15 19:08:53 -0300

    Add project search.

--- bin/gitlab-common.sh
+++ bin/gitlab-common.sh
@@ -8,6 +8,10 @@ resolve_origin_url() {
     echo $URL
 }
 
+resolve_api_url() {
+    echo $(resolve_origin_url)/api/v3/
+}
+
 get_private_token() {
     PRIVATE_TOKEN=$(git config --get gitlab.private-token)
     if [ "x$PRIVATE_TOKEN" != "x" ]; then
@@ -24,9 +28,9 @@ get_private_token() {
     read -s -p "Enter your gitlab password: " PASSWORD
     echo -e "\n"
 
-    ORIGIN_URL=$(resolve_origin_url)
+    API_URL="$(resolve_api_url)session"
     echo "Getting private token for user $USER from $ORIGIN_URL"
-    USER_DATA=`curl -skX POST "$ORIGIN_URL/api/v3/session?login=$USER&password=$PASSWORD"`
+    USER_DATA=`curl -skX POST "$API_URL?login=$USER&password=$PASSWORD"`
 
     PRIVATE_TOKEN=$($BASEDIR/gitlab-json.py "get_from" "$USER_DATA" "private_token")
 
@@ -41,3 +45,24 @@ get_private_token() {
     echo $PRIVATE_TOKEN
 }
 
+get_project_id() {
+    REMOTE_URL=$(git config --get remote.origin.url)
+    if [ $REMOTE_URL = "https*" ]; then
+        REMOTE_URL=$(echo $REMOTE_URL | grep "https" | sed -e "s/https\/\/://g")
+    fi
+    REMOTE_URL=$(echo $REMOTE_URL | grep ".git" | sed -e "s/\.git//g")
+    REMOTE_URL=$(echo $REMOTE_URL | grep ":" | sed -e "s/:/\//g")
+    NAMESPACE=$(echo $REMOTE_URL | grep "/" | cut -d/ -f2)
+    PROJECT=$(echo $REMOTE_URL | grep "/" | cut -d/ -f3)
+    ENCODED_SLASH="%2F"
+
+    PRIVATE_TOKEN=$(get_private_token)
+    API_URL="$(resolve_api_url)projects/${NAMESPACE}${ENCODED_SLASH}${PROJECT}"
+    PROJECT_DATA=`curl --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" -sk "$API_URL"`
+    echo $PROJECT_DATA
+}
+
+get_current_branch() {
+    CURRENT_BRANCH=$(git name-rev --name-only HEAD)
+    echo $CURRENT_BRANCH
+}
